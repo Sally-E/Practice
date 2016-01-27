cdcdbb7d61c5c343d064680293d64acecdb3e343
sally <sryendex@gmail.com>
2016-01-28 00:58:47 +0900

    first commit array add

---
+++ array-add_2.py
@@ -0,0 +1,17 @@
+# -*-coding: utf-8-*-
+
+import numpy as np
+
+line1 = input('please enter numbers for first array: ').split(",")
+line1 = list(map(int, line1))#入力した数列を文字の形からint型に変換
+print(line1)
+
+line2 = input('please enter numbers for second array: ').split(",")
+line2 = list(map(int, line2))
+print(line2)
+
+line1=np.array(line1)
+line2=np.array(line2)
+
+
+print (line1*line2)
